e0eea29d7a57ba6dca7d8ef1010cf9be32137a39
Rashid <rashidstanikzai.net@gmail.com>
2024-08-13 23:19:22 +0500

    feat: ordering added

--- src/wp-plugin/wp-builder.js
+++ src/wp-plugin/wp-builder.js
@@ -1,7 +1,8 @@
-import { readdir, readFile, writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import path, { join } from "path";
 import { JSDOM } from "jsdom";
 import { format } from "prettier";
+import { pathToFileURL } from "url";
 
 export default async function buildWp() {
   console.log("Applying wp...");
@@ -9,21 +10,41 @@ export default async function buildWp() {
   const sectionsDir = path.join(process.cwd(), "src/sections");
   const indexFile = path.join(process.cwd(), "index.html");
 
+  console.log(`looking for order.js file in ${sectionsDir}`);
+  const orderFile = path.join(sectionsDir, "order.js");
+  let orderArray;
   try {
-    const files = await readdir(sectionsDir);
+    const orderArrayModule = await import(pathToFileURL(orderFile).href);
+    orderArray = orderArrayModule.default;
+  } catch (err) {
+    console.log(`order.js file not found in ${sectionsDir}`);
+    console.log(err);
+  }
 
-    if (files.length <= 0) {
-      console.log("No files in sections directory detected");
-      return;
-    }
+  try {
+    // const files = await readdir(sectionsDir);
+    console.log(orderArray);
+
+    // if (files.length <= 0) {
+    //   console.log("No files in sections directory detected");
+    //   return;
+    // }
 
     let combinedContent = "";
 
-    for (const file of files) {
-      const path = join(sectionsDir, file);
-      const content = await readFile(path, "utf-8");
-      combinedContent += content + "\n";
-    }
+    await Promise.all(
+      orderArray.map(async (fileName) => {
+        const path = join(sectionsDir, fileName);
+        const content = await readFile(path, "utf-8");
+        combinedContent += content + "\n";
+      })
+    );
+
+    // for (const file of files) {
+    //   const path = join(sectionsDir, file);
+    //   const content = await readFile(path, "utf-8");
+    //   combinedContent += content + "\n";
+    // }
 
     let indexFileContent = await readFile(indexFile, "utf-8");
 
@@ -37,6 +58,10 @@ export default async function buildWp() {
     serializedIndexFile = await format(serializedIndexFile, { parser: "html" });
     await writeFile(indexFile, serializedIndexFile, "utf-8");
   } catch (err) {
-    console.log(err);
+    if (err.code === "ENOENT") {
+      console.log("File not found: ", err.path);
+    } else {
+      console.log("catch", err);
+    }
   }
 }
