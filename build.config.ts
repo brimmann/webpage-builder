e04b409c0ac626f065c542fe1172183855d1fc59
Rashid <rashidstanikzai.net@gmail.com>
2024-08-14 17:56:36 +0500

    fix: build file

--- build.config.ts
+++ build.config.ts
@@ -3,5 +3,10 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: ['./src/index'],
   clean: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+  },
+  failOnWarn: false,
   outDir: "dist"
 })
